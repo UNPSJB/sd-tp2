7a695c763edc3c96da1283b496f7655ca4e0317f
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-09-10 19:08:01 -0300

    Ejemplo temapltaes

--- start.py
+++ start.py
@@ -1,19 +1,22 @@
-from flas import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '''
-    <html>
-        <title>{titulo}</title>
-    <head>
-    </head>
-    <body>
-    {cuerpo}
-    </body>
-    </html>
-    '''.format(titulo="Hola mundo", cuerpo="Hola mundo")
+    return render_template(
+        "base.html",
+        title="Hola mundo",
+        variable="Una variable",
+    )
+
+
+@app.route('/agregar_usuario/', methods=["GET", "POST"])
+def agregar_usuario():
+
+    if request.method == 'POST':
+        print request.form
+    return render_template('agregar_usuario.html')
 
 
 def main():
