59ee6e437ba8f94c1a4a66448751476adc216cb2
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-09-11 17:23:03 -0300

    Abrir navegador automáticamente

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+from webbrowser import open_new_tab
+from thread import start_new_thread
+from functools import partial
 
 
 @app.route('/')
@@ -15,11 +18,18 @@ def index():
 def agregar_usuario():
 
     if request.method == 'POST':
-        print request.form
+        import ipdb; ipdb.set_trace()
+
     return render_template('agregar_usuario.html')
 
+def abrir(url):
+    from time import sleep
+    sleep(1)
+    print "Vamos a abrir"
+    open_new_tab(url)
 
 def main():
+    start_new_thread(abrir, ('http://localhost:5000/', ))
     app.run(debug=True)
 
 if __name__ == '__main__':
